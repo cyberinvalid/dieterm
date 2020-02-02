4ec2514d25508e4bf9196d6990d047288150fe78
@cyberinvalid <cyberinvalid@protonmail.com>
2020-02-02 20:50:16 +0200

    02.02.20 special

--- libs/core.mjs
+++ libs/core.mjs
@@ -1,3 +1,10 @@
+/*
+ *  Look at you, hacker. A pathetic creature of meat and bone, 
+ *  panting and sweating as you run through my corridors. 
+ *  How can you challenge a perfect, immortal machine?
+ *                                                          SHODAN
+ */
+
 import IO from './io.mjs';
 import DB from './db.mjs';
 import clear from './clear.mjs';
